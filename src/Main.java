b547eb8ebce75c62444b39b3e0ad5cf137d056de
hero0817 <cor9577@naver.com>
2025-08-17 20:51:49 +0900

    master kiwi

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("안녕하세요 마스터 브랜치입니다.");
         String a = "banana";
+        String b = "kiwi";
     }
 }
