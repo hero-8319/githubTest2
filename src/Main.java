42fa8353c5b53f46ae420781059f6ad079b45fdd
hero0817 <cor9577@naver.com>
2025-08-17 20:47:09 +0900

    mater banana

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요 히어로 브랜치입니다.");
-        String a = "orange";
+        System.out.println("안녕하세요 마스터 브랜치입니다.");
+        String a = "banana";
     }
 }
